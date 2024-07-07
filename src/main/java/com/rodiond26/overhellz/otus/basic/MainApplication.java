8d80186b83a4c72e6e138cf4d816050898000c89
rodiond26 <rodiond26@gmail.com>
2024-07-07 10:09:36 +0500

    add main

---
+++ src/main/java/com/rodiond26/overhellz/otus/basic/MainApplication.java
@@ -0,0 +1,7 @@
+package com.rodiond26.overhellz.otus.basic;
+
+public class MainApplication {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
---
+++ src/test/java/com/rodiond26/overhellz/otus/basic/MainApplicationTest.java
@@ -0,0 +1,4 @@
+package com.rodiond26.overhellz.otus.basic;
+
+class MainApplicationTest {
+}
