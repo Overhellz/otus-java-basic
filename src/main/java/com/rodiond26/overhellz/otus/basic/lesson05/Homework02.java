cbcf31829584e5c15f11df459a6a325e043e2276
rodiond26 <rodiond26@gmail.com>
2024-07-18 20:11:10 +0500

    HW02 решение ДЗ2

--- src/main/java/com/rodiond26/overhellz/otus/basic/lesson05/Homework02.java
+++ src/main/java/com/rodiond26/overhellz/otus/basic/lesson05/Homework02.java
@@ -14,21 +14,21 @@ public class Homework02 {
     }
 
     /**
-     * Метод печатает в консоль сумму всех элементов массива intArray, которые больше 5
+     * Метод печатает в консоль сумму всех элементов массива array, которые больше 5
      */
-    public static void printArraySum(int[] intArray) {
-        int sum = sumArrayIf(intArray, 5);
+    public static void printArraySum(int[] array) {
+        int sum = sumArrayIf(array, 5);
         System.out.println(sum);
     }
 
     /**
-     * Метод возвращает сумму всех элементов массива intArray, которые больше value
+     * Метод возвращает сумму всех элементов массива array, которые больше value
      */
-    public static int sumArrayIf(int[] intArray, int value) {
+    public static int sumArrayIf(int[] array, int value) {
         int sum = 0;
 
-        if (intArray != null) {
-            for (int elem : intArray) {
+        if (array != null) {
+            for (int elem : array) {
                 if (elem > value) {
                     sum += elem;
                 }
@@ -38,53 +38,51 @@ public class Homework02 {
     }
 
     /**
-     * Метод принимает массив intArray и заполняет его числом newValue
+     * Метод принимает массив array и заполняет его значением newValue
      */
-    public static void fillArray(int newValue, int[] intArray) {
-        if (intArray != null) {
-            for (int i = 0; i < intArray.length; i++) {
-                intArray[i] = newValue;
+    public static void fillArray(int newValue, int[] array) {
+        if (array != null) {
+            for (int i = 0; i < array.length; i++) {
+                array[i] = newValue;
             }
         }
     }
 
     /**
-     * Метод принимает массив intArray и увеличивает каждый его элемент на delta
+     * Метод принимает массив array и увеличивает каждый его элемент на delta
      */
-    public static void addValueToArrayElements(int delta, int[] intArray) {
-        if (intArray != null) {
-            for (int i = 0; i < intArray.length; i++) {
-                intArray[i] += delta;
+    public static void addValueToArrayElements(int delta, int[] array) {
+        if (array != null) {
+            for (int i = 0; i < array.length; i++) {
+                array[i] += delta;
             }
         }
     }
 
     /**
-     * Метод печатает в консоль сумму элементов левой или правой половины массива intArray в зависимости от того, которая больше
+     * Метод печатает в консоль сумму элементов левой или правой половины массива array в зависимости от того, которая больше
      */
-    public static void printGreaterHalfArraySum(int[] intArray) {
-        if (intArray == null || intArray.length == 0) {
+    public static void printGreaterHalfArraySum(int[] array) {
+        if (array == null || array.length == 0) {
             System.out.println(0);
             return;
         }
 
-        int sumLeft = sumArrayInRange(intArray, 0, intArray.length / 2);
-        int sumRight = sumArrayInRange(intArray, intArray.length / 2, intArray.length);
+        int sumLeft = sumArrayInRange(array, 0, array.length / 2);
+        int sumRight = sumArrayInRange(array, array.length / 2, array.length);
 
         System.out.println(Math.max(sumLeft, sumRight));
     }
 
     /**
-     * Метод возвращает сумму элементов массива intArray от элемента с индексом startIndex до элемента с индексом endIndex
+     * Метод возвращает сумму элементов массива array от элемента с индексом startIndex до элемента с индексом endIndex
      */
-    public static int sumArrayInRange(int[] intArray, int startIndex, int endIndex) {
+    public static int sumArrayInRange(int[] array, int startIndex, int endIndex) {
         int sum = 0;
-        System.out.println("startIndex: " + startIndex);
-        System.out.println("endIndex: " + endIndex);
 
-        if (intArray != null) {
-            for (int i = startIndex; i >= 0 && i < intArray.length; i++) {
-                sum += intArray[i];
+        if (array != null) {
+            for (int i = startIndex; i >= 0 && i < array.length && i < endIndex; i++) {
+                sum += array[i];
             }
         }
         return sum;
