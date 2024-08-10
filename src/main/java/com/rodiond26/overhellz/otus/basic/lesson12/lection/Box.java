b4e47fd30a6949a057f7179caeb785f2779730f7
rodiond26 <rodiond26@gmail.com>
2024-08-10 21:50:10 +0500

    добавил занятие 12

---
+++ src/main/java/com/rodiond26/overhellz/otus/basic/lesson12/lection/Box.java
@@ -0,0 +1,17 @@
+package com.rodiond26.overhellz.otus.basic.lesson12.lection;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Box {
+
+    private int size;
+
+    private int color;
+}
