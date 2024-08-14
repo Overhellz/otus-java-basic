b9a701d21ef0a4dccf8218590a2a2a910a03181c
rodiond26 <rodiond26@gmail.com>
2024-08-15 02:23:55 +0500

    HW08 добавил решение ДЗ8

---
+++ src/main/java/com/rodiond26/overhellz/otus/basic/lesson14/homework/exception/AppArrayNullException.java
@@ -0,0 +1,12 @@
+package com.rodiond26.overhellz.otus.basic.lesson14.homework.exception;
+
+public class AppArrayNullException extends RuntimeException {
+
+    public AppArrayNullException() {
+        super();
+    }
+
+    public AppArrayNullException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/rodiond26/overhellz/otus/basic/lesson14/homework/exception/AppArraySizeException.java
@@ -0,0 +1,12 @@
+package com.rodiond26.overhellz.otus.basic.lesson14.homework.exception;
+
+public class AppArraySizeException extends RuntimeException {
+
+    public AppArraySizeException() {
+        super();
+    }
+
+    public AppArraySizeException(String message) {
+        super(message);
+    }
+}
