466ade58e70a040d7b8294cc621203e8fd0dc355
rodiond26 <rodiond26@gmail.com>
2024-08-31 20:19:01 +0500

    откат

--- src/main/java/com/rodiond26/overhellz/otus/basic/utils/ConsolePrinter.java
+++ src/main/java/com/rodiond26/overhellz/otus/basic/utils/ConsolePrinter.java
@@ -1,14 +1,26 @@
 package com.rodiond26.overhellz.otus.basic.utils;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * Класс для вывода сообщений в консоль
+ */
 public final class ConsolePrinter {
 
     private ConsolePrinter() {
     }
 
+    /**
+     * Печатает в консоль message
+     */
     public static void log(String message) {
         System.out.println(message);
     }
 
+    /**
+     * Печатает в консоль message
+     */
     public static void logError(String message) {
         System.out.println("* " + message);
     }
