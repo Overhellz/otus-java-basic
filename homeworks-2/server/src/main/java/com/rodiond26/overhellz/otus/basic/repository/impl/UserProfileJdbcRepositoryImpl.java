d04343b493b1d8435858cb55fbf6efd908612cb6
rodiond26 <rodiond26@gmail.com>
2024-10-05 18:45:58 +0500

    HW18 продолжение решения ДЗ18

--- homeworks-2/server/src/main/java/com/rodiond26/overhellz/otus/basic/repository/impl/UserProfileJdbcRepositoryImpl.java
+++ homeworks-2/server/src/main/java/com/rodiond26/overhellz/otus/basic/repository/impl/UserProfileJdbcRepositoryImpl.java
@@ -2,15 +2,16 @@ package com.rodiond26.overhellz.otus.basic.repository.impl;
 
 import com.rodiond26.overhellz.otus.basic.config.PostgreSqlDbConfig;
 import com.rodiond26.overhellz.otus.basic.model.UserProfile;
+import com.rodiond26.overhellz.otus.basic.model.UserRole;
 import com.rodiond26.overhellz.otus.basic.repository.UserProfileRepository;
 import com.rodiond26.overhellz.otus.basic.repository.script.SqlInitScript;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+
+import static com.rodiond26.overhellz.otus.basic.repository.script.SqlCrudScript.*;
 
 public class UserProfileJdbcRepositoryImpl implements UserProfileRepository {
 
@@ -44,22 +45,81 @@ public class UserProfileJdbcRepositoryImpl implements UserProfileRepository {
 
     @Override
     public List<UserProfile> findAll() {
-        // TODO
-        return List.of();
+        List<UserProfile> resultList = new ArrayList<>();
+        try (Statement statement = connection.createStatement()) {
+            try (ResultSet resultSet = statement.executeQuery(SELECT_ALL_USERS)) {
+                while (resultSet.next()) {
+                    String login = resultSet.getString("login");
+                    String password = resultSet.getString("pass");
+                    String name = resultSet.getString("name");
+                    UserRole userRole = UserRole.valueOf(resultSet.getString("role"));
+                    resultList.add(new UserProfile(login, password, name, userRole));
+                }
+            }
+        } catch (SQLException e) {
+            System.err.println("Произошла ошибка при получении всех пользователей");
+            e.printStackTrace();
+        }
+        return resultList;
     }
 
     @Override
     public Optional<UserProfile> findByName(String name) {
+        try (PreparedStatement preparedStatement = connection.prepareStatement(FIND_USER_BY_NAME)) {
+            preparedStatement.setString(1, name);
+
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    String login = resultSet.getString("login");
+                    String password = resultSet.getString("pass");
+                    String userName = resultSet.getString("name");
+                    UserRole userRole = UserRole.valueOf(resultSet.getString("role"));
+                    return Optional.of(new UserProfile(login, password, userName, userRole));
+                }
+            }
+        } catch (SQLException e) {
+            System.err.println("Произошла ошибка при получении пользователя по имени: " + name);
+            e.printStackTrace();
+        }
         return Optional.empty();
     }
 
     @Override
     public Optional<UserProfile> findByLogin(String login) {
+        try (PreparedStatement preparedStatement = connection.prepareStatement(FIND_USER_BY_LOGIN)) {
+            preparedStatement.setString(1, login);
+
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    String userLogin = resultSet.getString("login");
+                    String password = resultSet.getString("pass");
+                    String userName = resultSet.getString("name");
+                    UserRole userRole = UserRole.valueOf(resultSet.getString("role"));
+                    return Optional.of(new UserProfile(userLogin, password, userName, userRole));
+                }
+            }
+        } catch (SQLException e) {
+            System.err.println("Произошла ошибка при получении пользователя по логину: " + login);
+            e.printStackTrace();
+        }
         return Optional.empty();
     }
 
     @Override
     public void add(UserProfile userProfile) {
+        try (PreparedStatement preparedStatement = connection.prepareStatement(INSERT_NEW_USER)) {
+            preparedStatement.setString(1, userProfile.getLogin());
+            preparedStatement.setString(2, userProfile.getPassword());
+            preparedStatement.setString(3, userProfile.getName());
+            preparedStatement.setString(4, userProfile.getRole().name());
 
+            int rowsInserted = preparedStatement.executeUpdate();
+            if (rowsInserted > 0) {
+                System.out.println("Добавлен новый пользователь: " + userProfile);
+            }
+        } catch (SQLException e) {
+            System.err.println("Произошла ошибка при добавлении пользователя: " + userProfile);
+            e.printStackTrace();
+        }
     }
 }
