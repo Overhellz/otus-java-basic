41d896ce1858c0d6313ee45028ed0540a69b7950
rodiond26 <rodiond26@gmail.com>
2024-10-22 01:05:01 +0500

    HW23 старт решения ДЗ23

--- web-1/october-http-server/src/main/java/ru/otus/october/http/server/HttpServer.java
+++ web-1/october-http-server/src/main/java/ru/otus/october/http/server/HttpServer.java
@@ -3,29 +3,33 @@ package ru.otus.october.http.server;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class HttpServer {
+
     private int port;
     private Dispatcher dispatcher;
+    private ExecutorService executorService;
 
-    public HttpServer(int port) {
+    public HttpServer(int port, int nThreads) {
         this.port = port;
         this.dispatcher = new Dispatcher();
+        this.executorService = Executors.newFixedThreadPool(nThreads);
     }
 
     public void start() {
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             System.out.println("Сервер запущен на порту: " + port);
-            try (Socket socket = serverSocket.accept()) {
-                byte[] buffer = new byte[8192];
-                int n = socket.getInputStream().read(buffer);
-                String rawRequest = new String(buffer, 0, n);
-                HttpRequest request = new HttpRequest(rawRequest);
-                request.info(true);
-                dispatcher.execute(request, socket.getOutputStream());
+            while (serverSocket.isBound() && !serverSocket.isClosed()) {
+                Socket socket = serverSocket.accept();
+                System.out.println("Подключен клиент на порту: " + socket.getPort());
+                executorService.execute(() -> new HttpConnectionWorker(socket, dispatcher).run());
             }
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            executorService.shutdown();
         }
     }
 }
